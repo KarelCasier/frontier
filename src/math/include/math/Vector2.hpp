2a3a13cf40174f5f9fb55a9ffcc84985688d8221
Karel Casier <KarelCasier@me.com>
2018-02-03 16:38:08 -0500

    Add more Vector2 math and tests

--- src/math/include/math/Vector2.hpp
+++ src/math/include/math/Vector2.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <type_traits>
+#include <cmath>
 
 namespace frontier {
 
@@ -27,6 +28,13 @@ public:
         return {static_cast<O>(_x), static_cast<O>(_y)};
     }
 
+    bool operator==(const Vector2& other) const
+    {
+        const auto xEqual{std::abs(_x - other._x) <= epsilon};
+        const auto yEqual{std::abs(_y - other._y) <= epsilon};
+        return xEqual && yEqual;
+    }
+
     /// Getters
     T x() const { return _x; };
     T y() const { return _y; };
@@ -35,12 +43,30 @@ public:
     void x(T x) { _x = std::move(x); };
     void y(T y) { _y = std::move(y); };
 
+    /// Scalar arithmetic
     template <typename S, typename = typename std::enable_if_t<std::is_arithmetic<S>::value>>
     Vector2 operator*(S scalar)
     {
         return {static_cast<T>(_x * scalar), static_cast<T>(_y * scalar)};
     }
+    template <typename S, typename = typename std::enable_if_t<std::is_arithmetic<S>::value>>
+    Vector2 operator/(S scalar)
+    {
+        assert(scalar != 0);
+        return {static_cast<T>(_x / scalar), static_cast<T>(_y / scalar)};
+    }
+    template <typename S, typename = typename std::enable_if_t<std::is_arithmetic<S>::value>>
+    Vector2 operator+(S scalar)
+    {
+        return {static_cast<T>(_x + scalar), static_cast<T>(_y + scalar)};
+    }
+    template <typename S, typename = typename std::enable_if_t<std::is_arithmetic<S>::value>>
+    Vector2 operator-(S scalar)
+    {
+        return {static_cast<T>(_x - scalar), static_cast<T>(_y - scalar)};
+    }
 
+    /// Scalar compound assignment arithmetic
     template <typename S, typename = typename std::enable_if_t<std::is_arithmetic<S>::value>>
     Vector2& operator*=(S scalar)
     {
@@ -48,13 +74,14 @@ public:
         _y *= scalar;
         return *this;
     }
-
     template <typename S, typename = typename std::enable_if_t<std::is_arithmetic<S>::value>>
-    Vector2 operator+(S scalar)
+    Vector2& operator/=(S scalar)
     {
-        return {static_cast<T>(_x + scalar), static_cast<T>(_y + scalar)};
+        assert(scalar != 0);
+        _x /= scalar;
+        _y /= scalar;
+        return *this;
     }
-
     template <typename S, typename = typename std::enable_if_t<std::is_arithmetic<S>::value>>
     Vector2& operator+=(S scalar)
     {
@@ -62,28 +89,67 @@ public:
         _y += scalar;
         return *this;
     }
+    template <typename S, typename = typename std::enable_if_t<std::is_arithmetic<S>::value>>
+    Vector2& operator-=(S scalar)
+    {
+        _x -= scalar;
+        _y -= scalar;
+        return *this;
+    }
 
+    /// Vector2 arithmetic
     Vector2 operator*(const Vector2& rhs) { return {_x * rhs._x, _y * rhs._y}; }
+    Vector2 operator/(const Vector2& rhs)
+    {
+        assert(rhs._x != 0);
+        assert(rhs._y != 0);
+        return {_x / rhs._x, _y / rhs._y};
+    }
+    Vector2 operator+(const Vector2& rhs) { return {_x + rhs._x, _y + rhs._y}; }
+    Vector2 operator-(const Vector2& rhs) { return {_x - rhs._x, _y - rhs._y}; }
 
+    /// Vector2 compound assignment arithmetic
     Vector2& operator*=(const Vector2& rhs)
     {
         _x *= rhs._x;
         _y *= rhs._y;
         return *this;
     }
-
-    Vector2 operator+(const Vector2& rhs) { return {_x + rhs._x, _y + rhs._y}; }
-
+    Vector2& operator/=(const Vector2& rhs)
+    {
+        assert(rhs._x != 0);
+        assert(rhs._y != 0);
+        _x /= rhs._x;
+        _y /= rhs._y;
+        return *this;
+    }
     Vector2& operator+=(const Vector2& rhs)
     {
         _x += rhs._x;
         _y += rhs._y;
         return *this;
     }
+    Vector2& operator-=(const Vector2& rhs)
+    {
+        _x -= rhs._x;
+        _y -= rhs._y;
+        return *this;
+    }
 
-    bool operator==(const Vector2& other) const { return _x == other._x && _y == other._y; }
+    /// Vector2 math
+    double length() { return std::sqrt((_x * _x) + (_y * +_y)); }
+    double lengthSquared() { return (_x * _x) + (_y * +_y); }
+    Vector2& normalize()
+    {
+        const auto l = length();
+        if (l != 0) {
+            (*this) *= 1 / l;
+        }
+        return *this;
+    }
 
 private:
+    static long double constexpr epsilon = 0.000001;
     T _x;
     T _y;
 };
--- src/math/test/test_Vector2.cpp
+++ src/math/test/test_Vector2.cpp
@@ -29,30 +29,47 @@ TEST(Vector2, Equal)
     ASSERT_EQ(a, b);
 }
 
-TEST(Vector2, AddVector2f)
+TEST(Vector2, FloatToIntCast)
 {
     /// Arrange
     Vector2<float> a{5.5, 2.2};
-    Vector2<float> b{6.6, 3.3};
 
     /// Act
-    auto result = a + b;
+    Vector2<int> b = static_cast<Vector2<int>>(a);
 
     /// Assert
-    ASSERT_EQ(result, Vector2<float>(12.1, 5.5));
+    ASSERT_EQ(b, Vector2<int>(5, 2));
 }
 
-TEST(Vector2, AddEqVector2f)
+
+/// Scalar arithmetic
+
+TEST(Vector2, MultiplyScalar)
 {
     /// Arrange
     Vector2<float> a{5.5, 2.2};
-    Vector2<float> b{6.6, 3.3};
+    float b{50.55};
 
     /// Act
-    a += b;
+    auto result = a * b;
 
     /// Assert
-    ASSERT_EQ(a, Vector2<float>(12.1, 5.5));
+    ASSERT_EQ(result, Vector2<float>(278.025, 111.21));
+}
+
+TEST(Vector2, DivideScalar)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+    float b{50.55};
+    float xResult = a.x() / b;
+    float yResult = a.y() / b;
+
+    /// Act
+    auto result = a / b;
+
+    /// Assert
+    ASSERT_EQ(result, Vector2<float>(xResult, yResult));
 }
 
 TEST(Vector2, AddScalar)
@@ -68,7 +85,50 @@ TEST(Vector2, AddScalar)
     ASSERT_EQ(result, Vector2<float>(56.05, 52.75));
 }
 
-TEST(Vector2, AddEqScalar)
+TEST(Vector2, SubtractScalar)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+    float b{50.55};
+
+    /// Act
+    auto result = a - b;
+
+    /// Assert
+    ASSERT_EQ(result, Vector2<float>(-45.05, -48.35));
+}
+
+/// Scalar compound assignment arithmetic
+
+TEST(Vector2, MultiplyAssignScalar)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+    float b{50.55};
+
+    /// Act
+    a *= b;
+
+    /// Assert
+    ASSERT_EQ(a, Vector2<float>(278.025, 111.21));
+}
+
+TEST(Vector2, DivideAssignScalar)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+    float b{50.55};
+    float xResult = a.x() / b;
+    float yResult = a.y() / b;
+
+    /// Act
+    a /= b;
+
+    /// Assert
+    ASSERT_EQ(a, Vector2<float>(xResult, yResult));
+}
+
+TEST(Vector2, AddAssignScalar)
 {
     /// Arrange
     Vector2<float> a{5.5, 2.2};
@@ -80,6 +140,22 @@ TEST(Vector2, AddEqScalar)
     /// Assert
     ASSERT_EQ(a, Vector2<float>(56.05, 52.75));
 }
+
+
+TEST(Vector2, SubtractAssignScalar)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+    float b{50.55};
+
+    /// Act
+    a -= b;
+
+    /// Assert
+    ASSERT_EQ(a, Vector2<float>(-45.05, -48.35));
+}
+
+/// Vector arithmetic
 
 TEST(Vector2, MultiplyVector2f)
 {
@@ -94,6 +170,51 @@ TEST(Vector2, MultiplyVector2f)
     ASSERT_EQ(result, Vector2<float>(36.3, 7.26));
 }
 
+TEST(Vector2, DivideVector2f)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+    Vector2<float> b{6.6, 3.3};
+    float xResult = a.x() / b.x();
+    float yResult = a.y() / b.y();
+
+    /// Act
+    auto result = a / b;
+
+    /// Assert
+    ASSERT_EQ(result, Vector2<float>(xResult, yResult));
+}
+
+TEST(Vector2, AddVector2f)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+    Vector2<float> b{6.6, 3.3};
+
+    /// Act
+    auto result = a + b;
+
+    /// Assert
+    ASSERT_EQ(result, Vector2<float>(12.1, 5.5));
+}
+
+TEST(Vector2, SubtractVector2f)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+    Vector2<float> b{6.6, 4.3};
+    float xResult = a.x() - b.x();
+    float yResult = a.y() - b.y();
+
+    /// Act
+    auto result = a - b;
+
+    /// Assert
+    ASSERT_EQ(result, Vector2<float>(xResult, yResult));
+}
+
+/// Vector compound assignment arithmetic
+
 TEST(Vector2, MultiplyEqVector2f)
 {
     /// Arrange
@@ -107,43 +228,91 @@ TEST(Vector2, MultiplyEqVector2f)
     ASSERT_EQ(a, Vector2<float>(36.3, 7.26));
 }
 
-TEST(Vector2, MultiplyScalar)
+TEST(Vector2, DivideEqVector2f)
 {
     /// Arrange
     Vector2<float> a{5.5, 2.2};
-    float b{50.55};
+    Vector2<float> b{6.6, 3.3};
+    float xResult = a.x() / b.x();
+    float yResult = a.y() / b.y();
 
     /// Act
-    auto result = a * b;
+    a /= b;
 
     /// Assert
-    ASSERT_EQ(result, Vector2<float>(278.025, 111.21));
+    ASSERT_EQ(a, Vector2<float>(xResult, yResult));
 }
 
-TEST(Vector2, MultiplyEqScalar)
+
+TEST(Vector2, AddEqVector2f)
 {
     /// Arrange
     Vector2<float> a{5.5, 2.2};
-    float b{50.55};
+    Vector2<float> b{6.6, 3.3};
 
     /// Act
-    a *= b;
+    a += b;
 
     /// Assert
-    ASSERT_EQ(a, Vector2<float>(278.025, 111.21));
+    ASSERT_EQ(a, Vector2<float>(12.1, 5.5));
 }
 
-TEST(Vector2, FloatToIntCast)
+
+TEST(Vector2, SubtractEqVector2f)
 {
     /// Arrange
     Vector2<float> a{5.5, 2.2};
+    Vector2<float> b{6.6, 4.3};
 
     /// Act
-    Vector2<int> b = static_cast<Vector2<int>>(a);
+    a -= b;
 
     /// Assert
-    ASSERT_EQ(b, Vector2<int>(5, 2));
+    ASSERT_EQ(a, Vector2<float>(-1.1, -2.1));
 }
+
+/// Vector2 math
+
+TEST(Vector2, LengthVector2f)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+	float expectedLength = std::sqrt(5.5 * 5.5 + 2.2 * 2.2);
+
+    /// Act
+	auto result = a.length();
+
+    /// Assert
+    ASSERT_EQ(result, expectedLength);
+}
+
+TEST(Vector2, LengthSquaredVector2f)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+	float expectedLength = (5.5 * 5.5 + 2.2 * 2.2);
+
+    /// Act
+	auto result = a.lengthSquared();
+
+    /// Assert
+    ASSERT_EQ(result, expectedLength);
+}
+
+TEST(Vector2, NormalizeVector2)
+{
+    /// Arrange
+    Vector2<float> a{5.5, 2.2};
+    Vector2<float> b{5.5, 2.2};
+
+    /// Act
+	a.normalize();
+
+    /// Assert
+    ASSERT_EQ(a, b * 1 / std::sqrt(5.5 * 5.5 + 2.2 * 2.2));
+}
+
+/// End Tests
 
 int main(int argc, char** argv)
 {
